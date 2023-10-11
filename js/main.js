c0dcc2790cfab359ed25a9ccec991a8abb06a5f7
AS-Coope <deltazirconium@gmail.com>
2023-10-11 18:28:50 -0500

    Add song name onscreen beside audio element

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 function changeSong() {
     let currentSong = document.getElementById("play-audio")
+    let currentSongName = document.getElementById("song-name")
+
     if (currentSong.getAttribute("src") === "audio/ZrC - Trap Test (130 bpm).mp3") {
         currentSong.setAttribute("src", "audio/ZrC - XLR8 [Snippet - 808 note change] (165bpm).mp3")
+        currentSongName.innerHTML = "XLR8 [Snippet - 808 note change] (165bpm)"
     } else if (currentSong.getAttribute("src") === "audio/ZrC - XLR8 [Snippet - 808 note change] (165bpm).mp3") {
         currentSong.setAttribute("src", "audio/ZrC - Trap Test (130 bpm).mp3")
+        currentSongName.innerHTML = "ZrC - Trap Test (130 bpm)"
     }
 }
