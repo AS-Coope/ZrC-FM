2d2d1a64d3ac22d14224a72faa69df0ca29a510e
AS-Coope <deltazirconium@gmail.com>
2023-12-17 07:53:49 -0500

    Make semantic changes across the entire js file

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let changeSongBtn = document.getElementById("change-song");
 const stations = {
-    "hip-hop": {
+    "hipHop": {
         "name": "Hip Hop",
         "songs": {
             1: {
                 url: "audio/Hip Hop/ZrC - Trap Test (130 bpm).mp3",
-                "song_name": "ZrC - Trap Test (130 bpm)"
+                "songName": "ZrC - Trap Test (130 bpm)"
             },
             2: {
                 url: "audio/Hip Hop/ZrC - XLR8 [Snippet - 808 note change] (165bpm).mp3",
-                song_name: "XLR8 [Snippet - 808 note change] (165bpm)"
+                "songName": "XLR8 [Snippet - 808 note change] (165bpm)"
             }
         }
     }
@@ -20,28 +20,28 @@ changeSongBtn.addEventListener('click', () => {
 });
 
 function changeSong() {
-    let currentSong = document.getElementById("play-audio")
-    let currentSongName = document.getElementById("song-name")
+    let currentSong = document.getElementById("play-audio");
+    let currentSongName = document.getElementById("song-name");
 
     if (currentSong.getAttribute("src") === "audio/ZrC - Trap Test (130 bpm).mp3") {
-        currentSong.setAttribute("src", "audio/ZrC - XLR8 [Snippet - 808 note change] (165bpm).mp3")
-        currentSongName.innerHTML = "XLR8 [Snippet - 808 note change] (165bpm)"
+        currentSong.setAttribute("src", "audio/ZrC - XLR8 [Snippet - 808 note change] (165bpm).mp3");
+        currentSongName.textContent = "XLR8 [Snippet - 808 note change] (165bpm)";
     } else if (currentSong.getAttribute("src") === "audio/ZrC - XLR8 [Snippet - 808 note change] (165bpm).mp3") {
-        currentSong.setAttribute("src", "audio/ZrC - Trap Test (130 bpm).mp3")
-        currentSongName.innerHTML = "ZrC - Trap Test (130 bpm)"
+        currentSong.setAttribute("src", "audio/ZrC - Trap Test (130 bpm).mp3");
+        currentSongName.textContent = "ZrC - Trap Test (130 bpm)";
     }
 }
 
 function main() {
 
     // get the number of songs in a specific station
-    let hip_hop_length = Object.values(stations["hip-hop"]["songs"]).length
-    let playlist_songs = document.getElementById("station-songs")
+    const hipHopLength = Object.values(stations["hipHop"]["songs"]).length;
+    let playlistSongs = document.getElementById("station-songs");
 
-    for (let numOfSongsInDirectory = 1; numOfSongsInDirectory <= hip_hop_length; numOfSongsInDirectory++) {
-        let song = document.createElement("p")
-        song.innerHTML = stations["hip-hop"]["songs"][numOfSongsInDirectory]["song_name"]
-        playlist_songs.append(song)
+    for (let numOfSongsInDirectory = 1; numOfSongsInDirectory <= hipHopLength; numOfSongsInDirectory++) {
+        let song = document.createElement("p");
+        song.textContent = stations["hipHop"]["songs"][numOfSongsInDirectory]["songName"];
+        playlistSongs.append(song);
     }
 }
 
